2e302d54e4f5e12416f664dbcf8c71c8caab5d30
Charul Yadav <84919243+Charulyadav@users.noreply.github.com>
2024-07-21 15:52:41 +0530

    Update BloodBankProjectApplicationTests.java

--- src/test/java/com/BloodBank/BloodBankProjectApplicationTests.java
+++ src/test/java/com/BloodBank/BloodBankProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BloodBankProjectApplicationTests {
+class BloodBankProjectApplicationTests{
 
 	@Test
 	void contextLoads() {
